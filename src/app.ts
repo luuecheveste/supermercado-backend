15c8b0c3dc65158d59dadbffbaa243e6d45d823c
luuecheveste <luecheveste11@gmail.com>
2025-11-27 20:06:33 -0300

    app.ts ok

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 app.use(
   cors({
     origin: 'https://supermercado-front-js.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   })
 );
