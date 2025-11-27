51c9128ba9a05cfd887ef0c1c069341a241d2590
luuecheveste <luecheveste11@gmail.com>
2025-11-27 00:16:30 -0300

    orm.ts listo

--- src/shared/orm.ts
+++ src/shared/orm.ts
@@ -1,16 +1,19 @@
 import { MikroORM } from '@mikro-orm/mysql';
 
 export const orm = await MikroORM.init({
+  // Entidades compiladas (dist)
   entities: ['dist/**/*.entity.js'],
+  // Entidades en desarrollo (ts)
   entitiesTs: ['src/**/*.entity.ts'],
 
-  clientUrl: process.env.DATABASE_URL,  // TiDB ya te da todo en la URL
+  // TiDB Serverless usa mysqls:// y requiere SSL sí o sí.
+  clientUrl: process.env.DATABASE_URL,
   dbName: 'supermercado',
 
-  //
   driverOptions: {
     ssl: {
-      rejectUnauthorized: false,   // OBLIGATORIO en Render + TiDB
+      rejectUnauthorized: false,  // NECESARIO en Render + TiDB
+      minVersion: 'TLSv1.2',
     },
   },
 
@@ -19,7 +22,6 @@ export const orm = await MikroORM.init({
   schemaGenerator: {
     disableForeignKeys: true,
     createForeignKeyConstraints: true,
-    ignoreSchema: [],
   },
 });
 
