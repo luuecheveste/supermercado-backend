5e223b3f64b2c22dd71a647e8deff41e67f87f7e
luuecheveste <luecheveste11@gmail.com>
2025-12-01 18:48:22 -0300

    producto.routes ok

--- src/producto/producto.route.ts
+++ src/producto/producto.route.ts
@@ -1,15 +1,16 @@
-// src/producto/producto.route.ts
 import { Router } from "express";
 import {sanitizeProductoInput, findAll, findOne, add, update, remove, countStock,
 findByNameStart, findByCategoriaStart} from "./producto.controler.js";
 
 export const productoRouter = Router();
 
-productoRouter.get("/", findAll);
-productoRouter.get("/:id", findOne);
-productoRouter.post("/", sanitizeProductoInput, add);
-productoRouter.put("/:id", sanitizeProductoInput, update);
-productoRouter.delete("/:id", remove);
 productoRouter.get("/stocktotal", countStock);
 productoRouter.get("/buscar", findByNameStart);
 productoRouter.get("/buscar-categoria", findByCategoriaStart);
+
+productoRouter.get("/", findAll);
+productoRouter.post("/", sanitizeProductoInput, add);
+productoRouter.put("/:id", sanitizeProductoInput, update);
+productoRouter.delete("/:id", remove);
+
+productoRouter.get("/:id", findOne);
